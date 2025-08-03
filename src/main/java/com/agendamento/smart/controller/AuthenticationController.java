7db259f2fda2a2ecb24c964c74c9758be3e5cc4d
Abmaellf <abmael.ninha@gmail.com>
2025-08-03 16:53:30 -0400

    fix: add WebConfiguratio and config SecurityConfiguration and AuthenticationController

--- src/main/java/com/agendamento/smart/controller/AuthenticationController.java
+++ src/main/java/com/agendamento/smart/controller/AuthenticationController.java
@@ -12,11 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
@@ -30,6 +28,7 @@ public class AuthenticationController {
     @Autowired
     private TokenService tokenService;
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
         System.out.println("register");
